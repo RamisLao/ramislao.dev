68dee979e9270bec6c1e5640bfabce605cb9aa8b
RamisLao <ramislao@hyperbeard.com>
2024-01-10 09:25:19 -0600

    MOVED: Main layout to RootLayout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,19 +1,62 @@
+'use client';
+
 import './globals.css';
+import styled from 'styled-components';
+
 import StyledComponentsRegistry from '@/lib/registry.lib';
 
-export const metadata = {
-	title: 'Ramis Lao',
-	description: 'ramislao.dev',
-};
+const MainContainer = styled.div`
+	position: relative;
+	height: 100vh;
+	width: 100%;
+`;
+
+const Frame = styled.div`
+	position: absolute;
+	overflow: hidden; // Clip the image if it goes outside the frame dimensions
+	background-repeat: no-repeat;
+	// Add other background properties as needed
+`;
+
+const TopFrame = styled(Frame)`
+	top: 0;
+	left: 0;
+	right: 0;
+	height: 193px; // Adjust as needed
+	background-image: url('/images/bg_top.png');
+	// Other background properties
+`;
+
+const LeftFrame = styled(Frame)`
+	top: 0;
+	left: 0;
+	bottom: 0;
+	width: 109px; // Adjust as needed
+	background-image: url('/images/bg_left.png');
+	// Other background properties
+`;
+
+const RightFrame = styled(Frame)`
+	top: 0;
+	right: 0;
+	bottom: 0;
+	width: 159px; // Adjust as needed
+	background-image: url('/images/bg_right.png');
+	// Other background properties
+`;
+
+const Content = styled.div`
+	overflow-y: scroll; // Enables scrolling for content
+`;
 
 export default function RootLayout({ children }) {
 	return (
 		<html lang='en'>
 			<head>
-				<title>{metadata.title}</title>
+				<title>{'Ramis Lao'}</title>
 				<meta
 					name='description'
-					content={metadata.description}
+					content={'ramislao.dev'}
 				/>
 				<meta
 					name='viewport'
@@ -29,7 +72,14 @@ export default function RootLayout({ children }) {
 				/>
 			</head>
 			<body>
-				<StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+				<StyledComponentsRegistry>
+					<MainContainer>
+						<TopFrame />
+						<LeftFrame />
+						<RightFrame />
+						<Content>{children}</Content>
+					</MainContainer>
+				</StyledComponentsRegistry>
 			</body>
 		</html>
 	);
